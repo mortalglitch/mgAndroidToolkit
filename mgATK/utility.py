6a77909b23f88446a218a051a3790a94b576d20a
Joshua Neeley <mortal.glitch@gmail.com>
2015-09-19 20:35:37 -0700

    Added simple text pusher utility to the utility script

--- mgATK/utility.py
+++ mgATK/utility.py
@@ -1,5 +1,6 @@
 import os
 import time
+import subprocess
 
 def buildUtilityMenu():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -10,7 +11,14 @@ def buildUtilityMenu():
     selection = input("Selection #: ")
     if(selection == 1):
         # Need to write the keyboard tool
-        print "TODO add keyboard tool"
+        simpleTextPusher()
         time.sleep(5)
     if(selection == 2):
         pass
+
+def simpleTextPusher():
+    rawText = raw_input("Enter text to push: ")
+    cleanText = rawText.replace(' ', '%s')
+    pushtextcmd = ("adb shell input keyboard text " + '"' + cleanText + '"')
+    pushtextresult = subprocess.check_output(pushtextcmd.split())
+    print pushtextresult.split('\r\n')
